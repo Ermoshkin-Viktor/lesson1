b94e2255f2a861c2dc616d059aec7aa2ae5bada5
15erm <15erm@mail.ru>
2023-02-21 18:50:17 +0300

    Изменения

--- GitApp/Models/ApplicationContext.cs
+++ GitApp/Models/ApplicationContext.cs
@@ -1,6 +1,17 @@
-﻿namespace GitApp.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GitApp.Models
 {
-    public class ApplicationContext
+    public class ApplicationContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();//создаем базу данных при первом обращении
+        }
+        public ApplicationContext()
+        {
+
+        }
     }
 }
--- GitApp/Models/User.cs
+++ GitApp/Models/User.cs
@@ -1,6 +1,15 @@
-﻿namespace GitApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GitApp.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Введите имя")]
+        [RegularExpression(@"\D*", ErrorMessage = "Вводите только буквы")]
+        public string Name { get; set; }//имя пользователя
+
+        [Required(ErrorMessage = "Введите возраст")]
+        public int Age { get; set; }//возраст пользователя
     }
 }
