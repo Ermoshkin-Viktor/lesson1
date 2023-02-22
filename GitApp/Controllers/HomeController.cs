97042c38f3d3ac203911b6563caaa689326ba4f2
15erm <15erm@mail.ru>
2023-02-22 09:51:57 +0300

    Создать код редактирования данных сотрудников

--- GitApp/Controllers/HomeController.cs
+++ GitApp/Controllers/HomeController.cs
@@ -23,5 +23,36 @@ namespace GitApp.Controllers
             //получаем объекты из бд создаем из них список и передаем в представление
             return View(await db.Users.ToListAsync());
         }
+
+        //выводим форму с данными об объекте
+        public async Task<IActionResult> Edit(int? id)
+        {
+            //если id не равен null
+            if (id != null)
+            {
+                //находим юзера по id
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                //если юзер не равен null
+                if (user != null)
+                    //выводим юзера в представление
+                    return View(user);
+            }
+            return NotFound();
+        }
+        //получаем отредактированные данные объекта
+        [HttpPost]
+        public async Task<IActionResult> Edit(User user)
+        {
+            //если модель валидна и не равна null
+            if (ModelState.IsValid && user != null)
+            {
+                //редактируем данные
+                db.Users.Update(user);
+                //выполняем обновление в бд
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return Content("Не корректные данные");
+        }
     }
 }
