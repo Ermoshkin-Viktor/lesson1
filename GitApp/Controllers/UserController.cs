3e17e456d83a6f4afae20b78f8668d29ae9767a6
Victor <15erm@mail.ru>
2023-02-23 12:08:38 +0300

    Добавил конструктор

--- GitApp/Controllers/UserController.cs
+++ GitApp/Controllers/UserController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using GitApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 namespace GitApp.Controllers
 {
     public class UserController : Controller
     {
+        ApplicationContext db;
+
+        public UserController(ApplicationContext db)
+        {
+            this.db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
         public IActionResult Index()
         {
             return View();
